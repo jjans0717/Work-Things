37986352e77a14204f235d2cdc57fecc5c16068f
jjans0717 <71025031+jjans0717@users.noreply.github.com>
2022-05-24 07:10:16 -0700

    Add files via upload

--- prompts.ps1
+++ prompts.ps1
@@ -1,38 +1,67 @@
 #Justin Jans ID#009457058
 
+#defines Menu Funtion
 Function Show-Menu{
-    param (
-        [string]$Title = 'Menu'
-    )
-    Clear-Host
-    Write-Host "=================== $Title ====================="
+    <#
+    .SYNOPSIS
+        Fucntion to display menu and collect input
+    #>
 
-    Write-Host "1: Press for this option"
-    Write-Host "2: Press for this option"
-    Write-Host "3: Press for this option"
-    Write-Host "4: Press for this option"
-    Write-Host "5: Press for this option"
+    $Menu = New-Object -TypeName System.Text.StringBuilder
+    [void]$Menu.AppendLine("...Menu Options....")
+    [void]$Menu.AppendLine("-------------------")
+    [void]$Menu.AppendLine("1. Option 1")
+    [void]$Menu.AppendLine("2. Option 2")
+    [void]$Menu.AppendLine("3. List Current CPU and Memory Usage")
+    [void]$Menu.AppendLine("4. Get Running Processes")
+    [void]$Menu.AppendLine("5. Exit")
+    [void]$Menu.AppendLine("-------------------")
+    Write-Host -ForegroundColor Cyan $Menu.ToString()
+    return $(Write-Host -ForegroundColor Cyan ">> Select a command (1-5)"; Read-Host)
 }
 
-
-do {
-    Show-Menu
-    $selection = Read-Host "Please Make a selection"
-    switch ($selection)
+#loop that calls menu and executes user options
+$UserInput = 1
+Try {
+    While ($UserInput -ne 5)
     {
-        '1' {
-            'You chose option 1'
-        }
-        '2' {
-            'You chose option 2'
-        }
-        '3' {
-            'You chose option 3'
-        }
-        '4' {
-            'You chose option1'
+        $UserInput = Show-Menu
+        switch ($UserInput)
+        {
+            1 #User chose option 1 
+            {
+                Write-Host 'You chose option 1'
+            }
+            2 #User chose option 2
+            {
+                'You chose option 2'
+            }
+            3 #List CPU and Memory usage
+            {
+                #variable that defines what we are looking for
+                $CounterList = "\Processor(_Total)\% Processor Time","\Memory\Committed Bytes"
+                #Get counters Interval 5 and Samples 4
+                Get-Counter $CounterList -SampleInterval 5 -MaxSamples 4
+            }
+            4 #Get running processes
+            {
+                Get-Process | Select-Object ID, Name, VM | Sort-Object Name | Out-GridView
+            }
         }
     }
-    pause
 }
-until ($selection -eq '5')
+
+#Out of Memory Error Handline
+Catch [System.OutOfMemoryException]
+{
+    Write-Host -ForegroundColor REd "System out of memory"
+}
+#Any other errors
+Catch
+{
+   Write-Host -ForegroundColor Red "Something Broke, sorry!"
+}
+Finally
+{
+
+}
